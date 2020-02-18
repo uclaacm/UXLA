5391ea92c596d76aba96e4327be930ba498bacf8
Kevin Tang <kevintang2023@gmail.com>
2020-02-17 19:13:03 -0800

    Added assets folder to eleventyConfig

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
     //Tell 11ty to search these directories for files
     eleventyConfig.addPassthroughCopy('src/css');
     eleventyConfig.addPassthroughCopy('src/pages');
+    eleventyConfig.addPassthroughCopy('src/assets');
 
     //make sure all html files in the pages directory uses the default layout
     //eleventyConfig.addDefaultLayout('src/pages/*.html', 'src/_includes/default.html');
